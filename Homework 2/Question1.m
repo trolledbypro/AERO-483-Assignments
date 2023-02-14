525a36af51729c0cdafae8b1c528f8b755c2a8ea
Zachary Zalass <zachzalass@gmail.com>
2023-02-13 22:01:12 -0500

    Finished question 1 parts 1 and 2

--- Homework 2/Question1.m
+++ Homework 2/Question1.m
@@ -7,7 +7,39 @@ clear;
 clc;
 format long g;
 
-% Define constants
-rho = [5000;7000;6000];
-r_not = [0;4000;0];
+%% Part 1
 
+
+% Define constants
+lambda = [5;7;6];
+r = [0;4;0];
+
+position = [r(1,1);r(2,1)];
+
+sat_a = [0;0];
+sat_b = [5; sqrt(11) + 4];
+sat_c = [-5;sqrt(11) + 4];
+
+sat_a_pos = sat_a - position;
+sat_b_pos = sat_b - position;
+sat_c_pos = sat_c - position;
+
+d_hats = [norm(sat_a_pos); norm(sat_b_pos); norm(sat_c_pos)];
+delta_lambda = lambda - d_hats;
+
+uv_a = sat_a_pos/d_hats(1,1);
+uv_b = sat_b_pos/d_hats(2,1);
+uv_c = sat_c_pos/d_hats(3,1);
+
+H = [-uv_a' 1; -uv_b' 1; -uv_c' 1];
+
+delta_r = inv(H'*H)*H'*delta_lambda;
+r_new = r + delta_r;
+
+%% Part 2
+
+L = 2 * eye(3);
+S = 10 * eye(3);
+
+delta_r = inv(H'*L*H + S)*H'*L*delta_lambda;
+r_new = r + delta_r;
